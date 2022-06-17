465bf458f0b279bdb2cfa4aab0efb58d6cbfc11b
anapriscilavieira <vieiraana05@gmail.com>
2022-06-17 18:22:25 -0300

    master

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
+//CSS
 import './App.css';
+
+//React
+import { useCallback, useEffect, useState } from 'react';
+
+//Data
+import { wordsList } from './data/word'
+
+//Components
 import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  {id: 1, name: 'start'},
+  {id: 2, name: 'game'},
+  {id: 3, name: 'end'}
+] 
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name)
+  const [words] = useState(wordsList)
+
+  // Start the secret word game
+  const startGame = () => {
+    setGameStage(stages[1].name)
+  }
+
+  // Process theletter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name)
+  }
+
+  // Restart the game
+
+  const retry = () => {
+    setGameStage(stages[0].name)
+  }
+  
   return (
     <div className="App">
-      <StartScreen />
+     {gameStage === 'start' && <StartScreen startGame={startGame}/>}
+     {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
+     {gameStage === 'end' && <GameOver retry={retry} />}
     </div>
   );
 }
 
 export default App;
+ 
